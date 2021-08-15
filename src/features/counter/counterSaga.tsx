f2b8e5526ab11bd4ed93e93d155abea66e3ecedb
Zendy <nguyenduc.051194@gmail.com>
2021-08-16 00:57:46 +0700

    takeEvery and takeLatest

--- src/features/counter/counterSaga.tsx
+++ src/features/counter/counterSaga.tsx
@@ -1,14 +1,26 @@
 import { PayloadAction } from "@reduxjs/toolkit";
-import { takeEvery } from "redux-saga/effects";
-import { increment } from "./counterSlice";
+import { delay, put, takeEvery, takeLatest } from "redux-saga/effects";
+import { increment, incrementSaga, incrementSagaSuccess } from "./counterSlice";
 
-export function* log(action: PayloadAction) {
-  yield console.log(`Log`, action);
+// export function* log(action: PayloadAction) {
+//   yield console.log(`Log`, action);
+// }
+
+function* handleIncrementSaga(action: PayloadAction<number>) {
+  console.log(`Waiting 1s`);
+  // Wait 1s
+  yield delay(1000)
+
+  console.log(`Waiting done, dispatch action`)
+  // Dispatch action success
+  yield put(incrementSagaSuccess(action.payload))
 }
 
 export default function* counterSaga() {
   yield console.log(`Counter saga`);
 
   // yield takeEvery('*', log);
-  yield takeEvery(increment().type, log);
+  // yield takeEvery(increment().type, log);
+  yield takeEvery(incrementSaga.toString(), handleIncrementSaga);
+  // yield takeLatest(incrementSaga.toString(), handleIncrementSaga);
 }
